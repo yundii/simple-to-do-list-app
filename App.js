8f14efaa88851b770a719b99a230015ab5979697
yundii <yunditao143@gmail.com>
2024-10-06 15:05:40 -0700

    Write App.js to show Activities screen and Diet screen in a bottom tab navigator

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import ActivitiesScreen from './Screens/Activities';
+import DietScreen from './Screens/Diet';
 
-export default function App() {
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const activitiesData = [
+  { id: '1', name: 'Yoga', date: 'Mon Sep 16 2024', value: '60 min' },
+  { id: '2', name: 'Weights', date: 'Mon Jul 15 2024', value: '120 min' },
+];
+
+const dietData = [
+  { id: '1', name: 'Breakfast', date: 'Tue Sep 17 2024', value: '500 cal' },
+  { id: '2', name: 'Lunch', date: 'Wed Sep 25 2024', value: '900 cal' },
+];
+
+function TabNavigator() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === 'Activities') {
+            iconName = 'walk';
+          } else if (route.name === 'Diet') {
+            iconName = 'fast-food';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tab.Screen name="Activities">
+        {() => <ActivitiesScreen data={activitiesData} />}
+      </Tab.Screen>
+      <Tab.Screen name="Diet">
+        {() => <DietScreen data={dietData} />}
+      </Tab.Screen>
+    </Tab.Navigator>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={TabNavigator} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
